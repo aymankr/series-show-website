9c4e8c1537ee61a75322905097b0523e38a7d147
Krrayman <kr.ayman@hotmail.com>
2022-01-15 11:58:33 +0100

    paginate seasons, episodes

--- src/Controller/SeriesController.php
+++ src/Controller/SeriesController.php
@@ -10,6 +10,7 @@ use App\Repository\SeriesRepository;
 use App\Repository\SeasonRepository;
 use App\Search\Search;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -56,13 +57,9 @@ class SeriesController extends AbstractController
     /**
      * @Route("/presentation/{id}", name="series_presentation", methods={"GET"})
      */
-    public function presentation(Series $serie, Request $request, SeasonRepository $seasonRepository): Response
+    public function presentation(Series $serie, Request $request, PaginatorInterface $paginator): Response
     {
-        $search = new Search();
-        $search->page = $request->get('page', 1);
-
-        $seasons = $seasonRepository->getSeasons($serie, $search);
-        dump($seasons);
+        $seasons = $paginator->paginate($serie->getSeasonsOrdered(), $request->query->getInt('page', 1), 5);
 
         return $this->render('series/presentation.html.twig', [
             'serie' => $serie,
@@ -73,14 +70,17 @@ class SeriesController extends AbstractController
      /**
      * @Route("/presentation/{id}/season/{number}", name="series_season_presentation", methods={"GET"})
      */
-    public function seasonsPresentation(int $id, int $number, SeriesRepository $seriesRepository): Response
+    public function seasonsPresentation(int $id, int $number, SeriesRepository $seriesRepository, PaginatorInterface $paginator, Request $request): Response
     {
         $serie = $seriesRepository->find($id);
         $season = $serie->getSeasons()[$number-1];
+        $episodes = $paginator->paginate($season->getEpisodesOrdered(), $request->query->getInt('page', 1), 8);
+
         return $this->render('series/season.html.twig', [
             'serie' => $serie,
             'seasonNumber' => $number,
-            'season' => $season
+            'season' => $season,
+            'episodes' => $episodes
         ]);
     }   
 
